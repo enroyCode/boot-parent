102b2ba256e74f67a9b99bb0b462025cb8b4eb03
zhuchao <zhuchao@hd123.com>
2018-08-17 10:40:10 +0800

    Initial commit

---
+++ boot-web/src/main/java/com/enroy/cloud/bootweb/BootWebApplication.java
@@ -0,0 +1,12 @@
+package com.enroy.cloud.bootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootWebApplication.class, args);
+  }
+}
